13409b10ffda2c5a50380d36144c084e3ad75e7d
Igor Kamyshev <garik.novel@gmail.com>
2018-04-02 14:49:22 +0700

    update work

--- data/work.js
+++ data/work.js
@@ -1,10 +1,17 @@
 export default [
     {
+        interval: 'апрель 2018 - сейчас',
+        title: 'Нетология',
+        subtitle: 'Методист',
+        description:
+            <p>Разрабатываю домашние задания для студентов</p>,
+    },
+    {
         interval: 'ноябрь 2016 - сейчас',
         title: 'QEEP-Pro',
         subtitle: 'Full-stack Web Developer',
         description:
-            <p>Пишу бизнес-логику и делаю интерфейсы CRM и интернет-магазинов</p>,
+            <p>Пишу бизнес-логику и делаю интерфейсы CRM и ERM на PHP (Symfony), JS (Express) или Scala (Play) и ReactJS / VueJS, управляю командой из 3 разработчиков (Scrum, Agile)</p>,
     },
     {
         interval: 'январь 2014 - ноябрь 2016',
