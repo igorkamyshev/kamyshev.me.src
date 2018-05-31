25ccfb6a9c59f59cf2e486a7f5e1373a2f726d6c
Igor Kamyshev <garik.novel@gmail.com>
2018-05-31 16:35:39 +0700

    Added information about last work place

--- data/work.js
+++ data/work.js
@@ -11,7 +11,10 @@ export default [
         title: 'QEEP-Pro',
         subtitle: 'Full-stack Web Developer',
         description:
-            <p>Пишу бизнес-логику и делаю интерфейсы CRM и ERM на PHP (Symfony), JS (Express) или Scala (Play) и ReactJS / VueJS, управляю командой из 3 разработчиков (Scrum, Agile)</p>,
+            <React.Fragment>
+                <p>Писал бизнес-логику и делал интерфейсы CRM и ERM на PHP (Symfony), JS (Express) или Scala (Play) и ReactJS / VueJS, управлял командой из 3 разработчиков (Scrum, Agile)</p>
+                <p>Участвовал в разработке <a target="_blank" href="https://procraft.com">Procraft.com</a> и <a target="_blank" href="https://qeep.pro">QEEP-Pro</a></p>
+            </React.Fragment>,
     },
     {
         interval: 'январь 2014 - ноябрь 2016',
