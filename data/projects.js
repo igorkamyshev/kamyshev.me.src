6471f39558d17deef95befbedd4940f8a0cf9611
Igor Kamyshev <igor@kamyshev.me>
2018-09-15 20:06:07 +0300

    Update projects data

--- data/projects.js
+++ data/projects.js
@@ -3,7 +3,7 @@ export default [
         type: 'personal',
         categories: ['Управление'],
         title: 'Holistic SMM Agency',
-        description: 'Образовательные каналы в Telegram. Суммарная аудитория порядка 17 тысяч.',
+        description: 'Образовательные каналы в Telegram. Суммарная аудитория порядка 20 тысяч.',
         image: 'projects/holistic-smm',
         link: 'https://holistic-smm.ru',
     },
