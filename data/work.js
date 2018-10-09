7b4b3ca431d44b89688a2eadfa3d888cb7ce5bf0
Igor Kamyshev <igor@kamyshev.me>
2018-10-09 11:27:10 +0300

    Update Works info

--- data/work.js
+++ data/work.js
@@ -5,9 +5,11 @@ export default [
         subtitle: 'Software Engineer',
         description:
             <React.Fragment>
-                <p>Разрабатываю <strong>бизнес-логику</strong> и делаю <strong>интерфейсы</strong> для веб-приложений.</p>
-                <p>Бэкенды на <strong>PHP</strong> или <strong>JS</strong>, фронтенды на <strong>React</strong>.</p>
-                <p>Организовал <strong>рефакторинг lagacy-кода</strong> <a href="https://faster.ru/">Faster.ru</a>. На бэкенде добился <strong>разделения уровней приложения</strong> (больше не делаем запросов к базе из шаблонов, да). Реализовал <strong>масштабируемую архитектуру</strong> фронтенда.</p>
+                <p>Разрабатываю бэкенды на <strong>PHP</strong> или <strong>JS</strong>, фронтенды на <strong>React</strong>.</p>
+                <p>
+                    Организовал <strong>рефакторинг lagacy-кода</strong> <a href="https://faster.ru/">Faster.ru</a>.
+                    На бэкенде добился <strong>разделения уровней приложения</strong> (больше не делаем запросов к базе из шаблонов, да).
+                    Реализовал <strong>масштабируемую архитектуру</strong> фронтенда.</p>
                 <p>Ввел практику написания <strong>тестов</strong> и код-ревью.</p>
                 <p><strong>Автоматизировал</strong> интеграцию и доставку приложения, настроил <strong>CI/CD</strong>.</p>
                 <p><strong>Руковожу командой</strong> из 3 разработчиков <strong>(Scrum, Agile)</strong>.</p>
@@ -19,7 +21,10 @@ export default [
         subtitle: 'Эксперт по JavaScript и React',
         description:
             <React.Fragment>
-                <p>Разрабатываю лекции и домашние задания для <a href="https://netology.ru/programs/react">курса по React</a>. Курирую дипломные проекты и проверяю домашние задания студентов.</p>
+                <p>
+                    Разрабатываю лекции и домашние задания для <a href="https://netology.ru/programs/react">курса по React</a>.
+                    Курирую дипломные проекты и проверяю домашние задания студентов.
+                </p>
                 <p>Веду лекции по <strong>интеграции React с другими бибилиотеками</strong>.</p>
             </React.Fragment>,
     },
@@ -29,10 +34,19 @@ export default [
         subtitle: 'Full-stack Web Developer',
         description:
             <React.Fragment>
-                <p>Разрабатывал <strong>бизнес-логику</strong> и делал <strong>интерфейсы</strong> приложений для внутренних нужд заказчиков.</p>
-                <p>Бэкенды на <strong>PHP</strong>, <strong>JS</strong>, иногда Scala, фронтенды на <strong>React</strong> и <strong>Vue</strong>.</p>
-                <p>Написал модуль статистики для <strong>мобильного приложения и веб-версии</strong> <a href="https://procraft.com">Procraft.com</a>. Интегрировал Google Analytics, разработал интерфейс с возможностью агрегировать и фильтровать данные.</p>
-                <p>Занимался <strong>поддержкой</strong> и развитием <a href="https://qeep.pro">QEEP-Pro</a> и побочных продуктов. Создал систему для генерации <strong>интернет-магазинов</strong>.</p>
+                <p>
+                    Разрабатывал бэкенды на <strong>PHP</strong>, <strong>JS</strong>, иногда Scala,
+                    фронтенды на <strong>React</strong> и Vue,
+                    мобильные приложения на <strong>React Native</strong>.
+                </p>
+                <p>
+                    Написал модуль статистики для <strong>мобильного приложения и веб-версии</strong> <a href="https://procraft.com">Procraft.com</a>.
+                    Интегрировал Google Analytics, разработал интерфейс и GraphQL API с возможностью агрегировать и фильтровать данные.
+                </p>
+                <p>
+                    Занимался <strong>поддержкой</strong> и развитием <a href="https://qeep.pro">QEEP-Pro</a> и побочных продуктов.
+                    Создал систему для генерации <strong>интернет-магазинов</strong>.
+                </p>
                 <p>Частично <strong>автоматизировал</strong> процесс код-ревью, настроил <strong>CI</strong>.</p>
                 <p><strong>Руководил командой</strong> из 3 разработчиков <strong>(Scrum, Agile)</strong>.</p>
             </React.Fragment>,
