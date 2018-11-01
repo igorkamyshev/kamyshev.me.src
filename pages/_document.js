a4debfe57431b1945476e69d09a938e6c780bcbe
Igor Kamyshev <igor@kamyshev.me>
2018-11-01 11:44:19 +0300

    Remove unused scripts and add defer to other

--- pages/_document.js
+++ pages/_document.js
@@ -42,36 +42,12 @@ export default class QeepDocument extends Document {
 
                     <div>
                         {/* Core JS Files */}
-                        <script src="/static/js/jquery-3.2.1.min.js" type="text/javascript"></script>
-                        <script src="/static/js/jquery-ui-1.12.1.custom.min.js" type="text/javascript"></script>
-                        <script src="/static/js/popper.js" type="text/javascript"></script>
-                        <script src="/static/js/bootstrap.min.js" type="text/javascript"></script>
+                        <script defer src="/static/js/jquery-3.2.1.min.js" type="text/javascript"></script>
+                        <script defer src="/static/js/jquery-ui-1.12.1.custom.min.js" type="text/javascript"></script>
+                        <script defer src="/static/js/popper.js" type="text/javascript"></script>
+                        <script defer src="/static/js/bootstrap.min.js" type="text/javascript"></script>
 
-                        {/* Switches */}
-                        <script src="/static/js/bootstrap-switch.min.js"></script>
-
-                        {/* Plugins for Slider */}
-                        <script src="/static/js/nouislider.js"></script>
-
-                        {/* Photoswipe files */}
-                        <script src="/static/js/photo_swipe/photoswipe.min.js"></script>
-                        <script src="/static/js/photo_swipe/photoswipe-ui-default.min.js"></script>
-                        <script src="/static/js/photo_swipe/init-gallery.js"></script>
-
-                        {/* Plugins for Select */}
-                        <script src="/static/js/bootstrap-select.js"></script>
-
-                        {/* for fileupload */}
-                        <script src="/static/js/jasny-bootstrap.min.js"></script>
-
-                        {/* Plugins for Tags */}
-                        <script src="/static/js/bootstrap-tagsinput.js"></script>
-
-                        {/* Plugins for DateTimePicker */}
-                        <script src="/static/js/moment.min.js"></script>
-                        <script src="/static/js/bootstrap-datetimepicker.min.js"></script>
-
-                        <script src="/static/js/paper-kit.js?v=2.1.0"></script>
+                        <script defer src="/static/js/paper-kit.js?v=2.1.0"></script>
                     </div>
 
                     <NextScript />
