d640a66f99c8af15414a8f59ba03f6a8a43276a8
Igor Kamyshev <igor@kamyshev.me>
2018-11-01 13:50:29 +0300

    fix typo

--- static/js/gtm.js
+++ static/js/gtm.js
@@ -1,6 +1,6 @@
 window.dataLayer = window.dataLayer || [];
 function gtag() {
-  window.dataLayer.dataLayer.push(arguments);
+  window.dataLayer.push(arguments);
 }
 gtag('js', new Date());
 gtag('config', 'UA-117862348-1');
