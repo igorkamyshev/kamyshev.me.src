6525003bc52c2a7b17d62ed52c7ab977d7cb919d
Igor Kamyshev <igor@kamyshev.me>
2018-11-01 14:04:53 +0300

    Remove unused script and styles

--- pages/_document.js
+++ pages/_document.js
@@ -20,12 +20,9 @@ export default class QeepDocument extends Document {
                     <link rel="apple-touch-icon" sizes="76x76" href="/static/img/apple-icon.png" />
                     <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-                    <meta name="google-site-verification" content="rkMUHPzS1QzK63X9HLLeatYU3hmLQxxLBKDgjlYu_RI" />
-
                     <title>Igor Kamyshev | Personal Site</title>
                     <meta name="description" content="Igor Kamyshev's presonal site"></meta>
 
-                    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
                     <meta name="viewport" content="width=device-width" />
 
                     <link href="/static/css/bootstrap.min.css" rel="stylesheet" />
@@ -35,27 +32,14 @@ export default class QeepDocument extends Document {
 
                     <link href='https://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css' />
                     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet" />
-                    <link href="/static/css/nucleo-icons.css" rel="stylesheet" />
 
+                    <meta name="google-site-verification" content="rkMUHPzS1QzK63X9HLLeatYU3hmLQxxLBKDgjlYu_RI" />
                     <link href="https://www.google-analytics.com" rel="preconnect" />
-
                     <script defer src="https://www.googletagmanager.com/gtag/js?id=UA-117862348-1"></script>
                     <script defer src="/static/js/gtm.js"></script>
                 </Head>
                 <body>
-            
                     <Main />
-
-                    <div>
-                        {/* Core JS Files */}
-                        <script defer src="/static/js/jquery-3.2.1.min.js" type="text/javascript"></script>
-                        <script defer src="/static/js/jquery-ui-1.12.1.custom.min.js" type="text/javascript"></script>
-                        <script defer src="/static/js/popper.js" type="text/javascript"></script>
-                        <script defer src="/static/js/bootstrap.min.js" type="text/javascript"></script>
-
-                        <script defer src="/static/js/paper-kit.js?v=2.1.0"></script>
-                    </div>
-
                     <NextScript />
                 </body>
             </html>
