213cf8ff5326295a74d0965db52ef3f92a4452e6
Igor Kamyshev <igor@kamyshev.me>
2018-11-11 00:44:05 +0700

    From bootstrap columns to custom grid

--- features/home/features/projects/molecules/Card/index.js
+++ features/home/features/projects/molecules/Card/index.js
@@ -5,22 +5,18 @@ import ExternalLink from '@site/ui/atoms/ExternalLink'
 import image from '@site/utils/image'
 import styles from './Card.css'
 
-const column = (index, length) =>
-  (index === length - 1 && length % 2 !== 0) ? 12
-    : (index % 4 === 0 || index % 4 === 3) ? 7 : 5
+// TODO: add margin-bottom: 0
 
-const Card = ({ card, index, arr }) => (
-  <div className={`col-md-${column(index, arr.length)}`} key={card.title}>
-    <div className={cx('card', styles.card)} data-background="image" style={{ backgroundImage: `url(${image(card.image)})` }}>
-      <ExternalLink href={card.link}>
-        <div className="card-body">
-          <h6 className="card-category">{card.categories.join(', ')}</h6>
-          <h3 className="card-title">{card.title}</h3>
-          <p className="card-description">{card.description}</p>
-          <br />
-        </div>
-      </ExternalLink>
-    </div>
+const Card = ({ card }) => (
+  <div className={cx('card', styles.card)} data-background="image" style={{ backgroundImage: `url(${image(card.image)})` }}>
+    <ExternalLink href={card.link}>
+      <div className="card-body">
+        <h6 className="card-category">{card.categories.join(', ')}</h6>
+        <h3 className="card-title">{card.title}</h3>
+        <p className="card-description">{card.description}</p>
+        <br />
+      </div>
+    </ExternalLink>
   </div>
 )
 
--- features/home/features/projects/organisms/Cards/index.js
+++ features/home/features/projects/organisms/Cards/index.js
@@ -5,13 +5,13 @@ import Description from '../../molecules/Description'
 
 import styles from './Cards.css'
 
+// TODO: add custom media for sm
+
 export default ({ projects }) =>
   <Container className={styles.container}>
     <Description />
 
-    <div className="row">
-      {projects.map((project, i) => (
-        <Card key={project.title} card={project} index={i} arr={project} />
-      ))}
-    </div>
+    <section className={styles.grid}>
+      {projects.map(project => <Card key={project.title} card={project} />)}
+    </section>
   </Container>
