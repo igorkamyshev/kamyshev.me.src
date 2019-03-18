274cacb99316b90fd24434c0cd15ff69e2cfcd78
Igor Kamyshev <igor@kamyshev.me>
2019-03-18 09:42:42 +0300

    feat: add redirects

--- src/code.js
+++ src/code.js
@@ -1,1 +1,14 @@
-console.log('new site')
+const articleCheckbox = document.getElementById('about-check')
+
+const redirect = urlPath => {
+  articleCheckbox.checked = urlPath.includes('articles')
+  window.history.pushState({}, '', urlPath)
+}
+
+articleCheckbox.addEventListener('change', ({ target }) => {
+  if (target.checked) {
+    redirect('/articles.html')
+  } else {
+    redirect('/')
+  }
+})
