ea06c626e2260a4ac3cfc51738121f5a1cb2dab7
Igor Kamyshev <igor@kamyshev.me>
2019-03-19 17:46:26 +0300

    perf: disable compression

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import { addDragHandler } from './interactive/addDragHandler'
 import { addArticleToggle } from './interactive/addArticleToggle'
 import { addCloseButtonsHandler } from './interactive/addCloseButtonsHandler'
 
-;(() => {
+const bootstrap = () => {
   addZhandler()
   addDragHandler()
   addArticleToggle()
   addCloseButtonsHandler()
-})()
+}
+
+bootstrap()
