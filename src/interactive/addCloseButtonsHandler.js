61c1ffcc3935cf3bd622552b88d462d42035a810
Igor Kamyshev <igor@kamyshev.me>
2019-03-19 16:16:39 +0300

    feat: add crosses

--- src/interactive/addCloseButtonsHandler.js
+++ src/interactive/addCloseButtonsHandler.js
@@ -1,12 +1,12 @@
 const closeButtons = document.querySelectorAll('[data-checker]')
 
-const listener = ({ target }) => {
-  const checker = document.getElementById(target.dataset.checker)
+const listener = button => () => {
+  const checker = document.getElementById(button.dataset.checker)
   checker.checked = false
 }
 
 export const addCloseButtonsHandler = () => {
   for (const button of closeButtons) {
-    button.addEventListener('click', listener)
+    button.addEventListener('click', listener(button))
   }
 }
