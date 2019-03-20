6354e6e59b3df002669372c45bae914da6609619
Igor Kamyshev <igor@kamyshev.me>
2019-03-20 10:55:26 +0300

    feat: add auto non breaking spacing

--- bundle/posthtml/typography.js
+++ bundle/posthtml/typography.js
@@ -2,6 +2,10 @@ const typography = str =>
   str
     .replace(/(“|"|&quot;)(.+?)(”|"|&quot;)/gi, (_1, _2, inner) => `«${inner}»`)
     .replace(/(\s*?)(—)(\s*)/gi, ` — `)
+    .replace(
+      /\s+.{1,2}(\s+)/gi,
+      match => `${match.trimEnd()}${String.fromCharCode(160)}`,
+    )
 
 module.exports.typography = tree =>
   tree.walk(i => {
