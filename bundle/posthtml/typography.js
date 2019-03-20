b1de5cee95c82d9b2bdbc2fa6b7600a1d9df7388
Igor Kamyshev <igor@kamyshev.me>
2019-03-20 10:36:30 +0300

    feat: add auto typography

--- bundle/posthtml/typography.js
+++ bundle/posthtml/typography.js
@@ -1,10 +1,14 @@
+const typography = str =>
+  str
+    .replace(/(“|"|&quot;)(.+?)(”|"|&quot;)/gi, (_1, _2, inner) => `«${inner}»`)
+    .replace(/(\s*?)(—)(\s*)/gi, ` — `)
+
 module.exports.typography = tree =>
   tree.walk(i => {
     if (i.content) {
       const newContent = i.content.map(c => {
         if (typeof c === 'string') {
-          // TODO: add
-          return c
+          return typography(c)
         }
 
         return c
