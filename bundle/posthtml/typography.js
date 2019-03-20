71e6ae5bf6a67294ff8108bebfed5b695f2757e8
Igor Kamyshev <igor@kamyshev.me>
2019-03-20 11:19:28 +0300

    fix: fix auto spacing

--- bundle/posthtml/typography.js
+++ bundle/posthtml/typography.js
@@ -1,11 +1,16 @@
+const NON_BREAKING_SPACE = String.fromCharCode(160)
+
 const typography = str =>
   str
     .replace(/(“|"|&quot;)(.+?)(”|"|&quot;)/gi, (_1, _2, inner) => `«${inner}»`)
     .replace(/(\s*?)(—)(\s*)/gi, ` — `)
-    .replace(
-      /\s+.{1,2}(\s+)/gi,
-      match => `${match.trimEnd()}${String.fromCharCode(160)}`,
-    )
+    .replace(/\s+.{1,2}(\s+)/gi, match => {
+      if (match && match.trimEnd) {
+        return `${match.trimEnd()}${NON_BREAKING_SPACE}`
+      }
+
+      return NON_BREAKING_SPACE
+    })
 
 module.exports.typography = tree =>
   tree.walk(i => {
