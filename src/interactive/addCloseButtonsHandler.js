8a14bb87e367bfb8123a8d55aaccf71960eafb84
Igor Kamyshev <igor@kamyshev.me>
2019-03-21 15:07:55 +0300

    fix: fix one more accessible bug

--- src/interactive/addCloseButtonsHandler.js
+++ src/interactive/addCloseButtonsHandler.js
@@ -1,8 +1,12 @@
+import { recalculateCardsState } from './actions/recalculateCardsState'
+
 const closeButtons = document.querySelectorAll('[data-checker]')
 
 const listener = button => () => {
   const checker = document.getElementById(button.dataset.checker)
   checker.checked = false
+
+  recalculateCardsState(checker.dataset.card)
 }
 
 export const addCloseButtonsHandler = () => {
