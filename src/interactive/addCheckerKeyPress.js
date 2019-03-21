b25eef218581c861c025d9528b2de653f028843a
Igor Kamyshev <igor@kamyshev.me>
2019-03-21 15:06:33 +0300

    fix: fix bug with recalculate state on key press

--- src/interactive/addCheckerKeyPress.js
+++ src/interactive/addCheckerKeyPress.js
@@ -1,8 +1,11 @@
+import { recalculateCardsState } from './actions/recalculateCardsState'
+
 const labels = document.getElementsByTagName('label')
 
 const toggle = event => {
   const checkbox = document.getElementById(event.target.htmlFor)
   checkbox.checked = !checkbox.checked
+  recalculateCardsState(checkbox.dataset.card)
 }
 
 export const addCheckerKeyPress = () => {
