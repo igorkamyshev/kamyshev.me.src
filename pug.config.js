f8015787362cd975a47a1a1dcd3043ae82b778f5
Igor Kamyshev <igor@kamyshev.me>
2019-03-25 22:24:36 +0300

    build(mmarkdown): replace manualy markdown by auto

---
+++ pug.config.js
@@ -0,0 +1,15 @@
+const { readdirSync, readFileSync } = require('fs')
+const { resolve } = require('path')
+
+const articles = readdirSync(resolve(__dirname, 'data', 'articles'))
+  .filter(file => file.endsWith('md'))
+  .map(file => resolve(__dirname, 'data', 'articles', file))
+  .map(file => readFileSync(file).toString())
+  .reverse()
+
+module.exports = {
+  locals: {
+    articles,
+    md: require('marked'),
+  },
+}
