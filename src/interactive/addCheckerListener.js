e248aacbcf50aab197f14d2b8de5b7aba87c3b37
Igor Kamyshev <igor@kamyshev.me>
2019-06-25 22:50:04 +0300

    refactor: simplify code

--- src/interactive/addCheckerListener.js
+++ src/interactive/addCheckerListener.js
@@ -2,12 +2,10 @@ import { recalculateCardsState } from './actions/recalculateCardsState'
 
 const checkers = document.getElementsByTagName('input')
 
-const listener = cheker => () => {
-  recalculateCardsState(cheker.dataset.card)
-}
-
 export const addCheckerListener = () => {
   for (const cheker of checkers) {
-    cheker.addEventListener('change', listener(cheker))
+    cheker.addEventListener('change', () =>
+      recalculateCardsState(cheker.dataset.card),
+    )
   }
 }
--- src/interactive/addCloseButtonsHandler.js
+++ src/interactive/addCloseButtonsHandler.js
@@ -2,12 +2,8 @@ import { closeCard } from './actions/closeCard'
 
 const closeButtons = document.querySelectorAll('.card__close')
 
-const listener = button => () => {
-  closeCard(button.dataset.checker)
-}
-
 export const addCloseButtonsHandler = () => {
   for (const button of closeButtons) {
-    button.addEventListener('click', listener(button))
+    button.addEventListener('click', () => closeCard(button.dataset.checker))
   }
 }
