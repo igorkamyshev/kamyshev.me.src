a8d8928fa6667e349c2a6754a998ba3402e52387
Igor Kamyshev <igor@kamyshev.me>
2019-07-09 18:52:20 +0300

    chore: add script for new article part2

---
+++ utils/helpers/generateName.js
@@ -0,0 +1,14 @@
+const CyrillicToTranslit = require('cyrillic-to-translit-js')
+const transliterator = new CyrillicToTranslit()
+
+const generateName = header =>
+  transliterator
+    .transform(header)
+    .trim()
+    .replace(/\s+/gi, '-')
+    .replace(/\./gi, '')
+    .toLowerCase()
+
+module.exports = {
+  generateName,
+}
